1882b88076041147d2ea08f23e8518203b0142de
Darren Rambaud <xyzst@users.noreply.github.com>
2018-10-31 20:40:11 -0500

    Added StorePicker component

---
+++ src/components/StorePicker.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class StorePicker extends React.Component {
+  render() {
+    return <p>hello, m8!</p>
+  }
+}
+
+export default StorePicker;
